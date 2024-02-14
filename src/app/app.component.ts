8fd6a558da17f1a2a061110d455f36afd5eaa4a6
Andrii Khomiak <an.khomiak@gmail.com>
2024-02-14 20:21:45 +0200

    task done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,51 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { BoxesComponent } from './components/boxes/boxes.component';
+import { PasswordStatuses } from './types/PasswordStatuses';
+
+function calculatePasswordStrength(password: string) {
+  if (password.length === 0) {
+    return PasswordStatuses.Empty;
+  }
+
+  if (password.length < 8) {
+    return PasswordStatuses.LessThen8chars;
+  }
+
+  const hasDigits = /\d/.test(password);
+
+  const hasLetters = password
+    .split('')
+    .some(ch => ch.toLowerCase() !== ch.toUpperCase());
+
+  const hasSymbols = password
+    .split('')
+    .some(ch => /\D/.test(ch) && ch.toLowerCase() === ch.toUpperCase());
+
+  switch (true) {
+    case (hasLetters && hasDigits && hasSymbols):
+      return PasswordStatuses.Strong;
+
+    case (hasLetters && hasDigits) || (hasLetters && hasSymbols) || (hasDigits && hasSymbols):
+      return PasswordStatuses.Medium
+
+    default:
+      return PasswordStatuses.Easy
+  }
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, BoxesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'password-checker';
+  password = '';
+
+  get passwordStrength() {
+    return calculatePasswordStrength(this.password);
+  }
 }
